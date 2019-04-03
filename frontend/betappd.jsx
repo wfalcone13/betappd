73f9eef3da80ce9db5de35b674c653a927683bfb
William Falcone <wjf0003@gmail.com>
2019-04-03 10:58:23 -0400

    saved

--- frontend/betappd.jsx
+++ frontend/betappd.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import {signup, logout, login} from '../frontend/util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () =>{
+  window.login = login
+  window.logout = logout 
+  window.signup = signup
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Hi BeTappd</h1>, root)
 })
