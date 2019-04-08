60140bac51b3b531971ca84fdc050d777eaf309b
William Falcone <wjf0003@gmail.com>
2019-04-08 00:15:50 -0400

    images

--- frontend/components/greeting/splash.jsx
+++ frontend/components/greeting/splash.jsx
@@ -14,15 +14,28 @@ class SessionLinks extends React.Component{
     this.state = this.props.user 
   }
 
+ 
+
 render(){
    {
   return (
 
     <div className="main-div">
-      <div>
-        <button><Link to="/login">Sign In</Link></button>
-        <button><Link to="/create">Create An Account</Link></button>
-      </div>
+        <div className="sign-links">
+        {/* <button className='link-login'><Link to="/login">Sign In</Link></button> */}
+        {/* <button className='link-create'><Link to="/create" >Create An Account</Link></button> */}
+        <Link to="/login" className='link-login'>SIGN IN</Link>
+        <Link to="/create" className='link-create' >CREATE AN ACCOUNT</Link>
+        </div>
+        <div>
+          <div>
+            <p1>placeholder</p1>
+          </div>
+          <div className='phone-img'>
+          
+          </div>
+        </div>
+ 
       <div>
 
       </div>
